d7c0e80f209985174317cd162fa461be8110a1d7
Kanchan Singh <kanchan@kanchan.singhrautel>
2022-05-01 16:02:08 +0530

    Added Navigation Between Modules

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.googlenews.SplashFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
